0fb63807092c2e083f648522b2c6a8661d07df85
cdelahoze <cdelahoze@hotmail.com>
2022-09-24 20:40:11 -0500

    Teams W02

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const lista = document.getElementById('lista')
 const arrayElement = [ 
 {label: 'Week 1 notes', url: 'W01/index.html'},
 {label: 'Week 2', url: 'https://byui-cit.github.io/cit261/resources/portfolio.html'},
+{label: 'Week 2 team', url: 'W02/Teams/index2.html'},
 {label: 'Week 3', url: 'https://byui-cit.github.io/cit261/resources/portfolio.html'}
 ]
 
